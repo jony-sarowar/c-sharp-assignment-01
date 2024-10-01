c593a89e9e0bdb34e7e332abad6949f3ed930bb7
Jony Sarowar <42883973+jony-sarowar@users.noreply.github.com>
2024-10-01 09:22:24 +0600

    Add files via upload

---
+++ assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string productName = "Samsung Mobile";
+        double productPrice = 320.5;
+        string category = "Smart phone";
+        bool isAvailable = true;
+        int productSold = 5;
+
+        Console.Write("Name :");
+        Console.WriteLine(productName);
+        Console.Write("Pricr :");
+        Console.WriteLine(productPrice);
+        Console.Write("Category :");
+        Console.WriteLine(category);
+        Console.Write("Available :");
+        Console.WriteLine(isAvailable);
+        Console.Write("Sold :");
+        Console.WriteLine(productSold);
+    }
+}
+
